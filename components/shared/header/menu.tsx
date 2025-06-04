b0d3fff7e96004a320e485321c4466b6ccd0b46d
UdynGP <udyngpgate2023@gmail.com>
2025-06-05 01:01:31 +0530

    Initial commit

---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import ModeToggle from "./mode-toggle";
+import Link from "next/link";
+import {
+  EllipsisVertical,
+  ShoppingCart,
+  ShoppingCartIcon,
+  UserIcon,
+} from "lucide-react";
+
+const Menu = () => {
+  return (
+    <div className="flex justify-end gap-3">
+      <nav className="hidden md:flex w-full max-w-xs gap-1">
+        <ModeToggle />
+        <Button asChild variant="ghost">
+          <Link href="/cart">
+            <ShoppingCart /> Cart
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link href="/cart">
+            <UserIcon /> Sign In
+          </Link>
+        </Button>
+      </nav>
+      <nav className="md:hidden">
+        <Sheet>
+          <SheetTrigger className="align-middle">
+            <EllipsisVertical />
+          </SheetTrigger>
+          <SheetContent className="flex flex-col items-start p-6">
+            <SheetTitle>Menu</SheetTitle>
+            <ModeToggle />
+            <Button asChild variant="ghost">
+              <Link href="/cart">
+                <ShoppingCartIcon /> Cart
+              </Link>
+            </Button>
+            <Button asChild>
+              <Link href="/cart">
+                <UserIcon /> Sign In
+              </Link>
+            </Button>
+            <SheetDescription></SheetDescription>
+          </SheetContent>
+        </Sheet>
+      </nav>
+    </div>
+  );
+};
+
+export default Menu;
---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  // ignoring extra files and folders
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "dist/**",
+      "out/**",
+      "build/**",
+      "lib/generated/**", // <--- this miserable new-way folder of Prisma Client :-)
+    ],
+  }, //
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    rules: { "@typescript-eslint/no-explicit-any": "off" },
+  }),
+];
+
+export default eslintConfig;
